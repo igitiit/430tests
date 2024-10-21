a2a5d210f9214378aadadd38b1b7eb08f9e9c01d
James Papademas <jpapadem@iit.edu>
2024-10-21 12:45:11 -0500

    new yaml 2

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3w_x&e0jcy5)7_-%l_z=zreuv7b8+0f%082w*$+=&5g21+le4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
+DEBUG = int(os.environ.get('DEBUG', default=0))
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -79,10 +83,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog_db',
-        'USER': 'root',
-        'PASSWORD': 'jp',
-        'HOST': 'localhost',
+        'USER': 'coursera',
+        'PASSWORD': 'coursera',
+        'HOST': 'coursera-mysql-instance2.cr2ygeme6l7y.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 }
 
@@ -127,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['34.207.242.131', 'ec2-34-207-242-131.compute-1.amazonaws.com']
